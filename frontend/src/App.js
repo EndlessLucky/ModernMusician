069ca4c563c305d6d2a081c103f073b531fe20ae
Olive105 <105.olivesoft@gmail.com>
2022-03-16 22:07:33 -0400

    feat - implement footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from './layout/Header';
+import Footer from './layout/Footer';
 
 function App() {
   return (
     <Router>
       <div className='main-container m-auto'>
         <Header/>
+        <Footer/>
       </div>
     </Router>
   );
--- frontend/src/layout/Footer.js
+++ frontend/src/layout/Footer.js
@@ -1,7 +1,22 @@
 export default function Footer(){
   return(
-    <div className='header'>
-      
-    </div>
+    <div className='d-flex mt-3'>
+      <div className='footer d-flex'>
+        <img src='Silver_Fan_Tier.png' alt='' width='61px' height='64px' />
+        <div className='col' style={{'margin-left': '20px'}}>
+          <p className='text-white font-18 m-0'>Michael Walker</p>
+          <p className='text-white font-13 m-0'>
+            You’re only 75 points away from unlocking Gold!
+          </p>
+          <a className='text-white m-0' href='/'>
+            Click here to earn more StreetTeam Points
+          </a>        
+        </div>      
+      </div>
+      <div className='point-block text-center'>
+        <p className='text-white font-face-om font-30 m-0'>120</p>
+        <p className='text-white'>POINTS</p>
+      </div>
+    </div>    
   )
 }
