864e8d469f8d01836ae0f4806a78f41b69ccf837
Olive105 <105.olivesoft@gmail.com>
2022-03-16 22:42:25 -0400

    feat - implement Waitlist page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from './layout/Header';
 import Footer from './layout/Footer';
+import Waitlist from './page/Waitlist';
 
 function App() {
   return (
     <Router>
       <div className='main-container m-auto'>
         <Header/>
+          <Routes>
+            <Route exact path="/" element={<Waitlist />} />
+          </Routes>
         <Footer/>
       </div>
     </Router>
---
+++ frontend/src/page/Waitlist.js
@@ -0,0 +1,34 @@
+export default function Waitlist() {
+  return(
+    <div className='content-container m-auto'>
+      <h1 className='text-white font-face-om text-center font-60 mt-5'>
+        Own Music NFTs
+      </h1>
+      <h3 className='text-white font-face-om font-40 text-center'>
+        From Cue No Ego
+      </h3>
+      <div className='text-center mt-5'>
+        <video width="100%" height="auto" controls autoPlay>
+          <source src="" type="video/mp4"/>
+        </video>
+      </div>      
+      <button className='main-button font-face-om font-26 border-none mt-4'>
+        Get a FREE NFT - Register Now
+      </button>
+      <div className='row mt-5'>
+        <div className='col-md-6'>
+          <img src='Box_Bronze.jpg' alt='' width='100%' height='auto' />
+          <button className='sub-button font-face-om font-22 border-none mt-3'>
+            FREE (100 only)
+          </button>
+        </div>
+        <div className='col-md-6'>
+          <img src='Box_Diamond.jpg' alt='' width='100%' height='auto' />
+          <button className='sub-button font-face-om font-22 border-none mt-3'>
+            DIAMOND (1 only)
+          </button>
+        </div>
+      </div>
+    </div>    
+  )
+}
