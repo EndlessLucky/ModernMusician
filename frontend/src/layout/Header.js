c1a9af15497669a193f026238bac5e46ebda8f92
Olive105 <105.olivesoft@gmail.com>
2022-03-16 22:58:55 -0400

    feat - add home link

--- frontend/src/layout/Header.js
+++ frontend/src/layout/Header.js
@@ -1,9 +1,12 @@
 import { Dropdown } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return(
     <div className='header d-flex justify-content-between'>
-      <img src='./Square_-_Bright.png' alt='' width='38px' height='38px'/>
+      <Link to="/">
+        <img src='./Square_-_Bright.png' alt='' width='38px' height='38px'/>
+      </Link>      
       <Dropdown>
         <Dropdown.Toggle id="role-dropdown" className="bg-transparent border-none font-28 font-face-om pt-0 pb-0">
           RELICS
